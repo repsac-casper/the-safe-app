5e180f31b9a954f66165ecf55e97bd00f97fd44b
repsac-casper <124305750+repsac-casper@users.noreply.github.com>
2024-03-12 09:08:44 +0100

    v?

--- geld.js
+++ geld.js
@@ -1,31 +1,28 @@
-const overmaken = document.getElementById('first_content_btn');
-share.addEventListener('click',function(event){
-  event.preventDefault()
-  const bedrag = getElementById('bedrag')
-  // This is your test secret API key.
-    const stripe = require('stripe')('sk_test_51OohMaHQSNvOpQERrP6aiRgdOiMpEKGMhrM1TOWul5sXqEupoiyVHznyHb856g3upvzyOkf9dObPwI01iDulGcNy00DJNHaxQE');
-    const express = require('express');
-    const app = express();
-    app.use(express.static('public'));
+(function() {
+  var stripe = Stripe('pk_live_51OohMaHQSNvOpQERT5xJ1p5YOIuDSf4c6xiRglH8lGgEIGzC1chWV9QEuKX9S2V6bKhn63mfeAaYZebaONOuUgTs00X4NLNNKH');
 
-    const YOUR_DOMAIN = 'https://repsac-casper.github.io/the-safe-app';
-
-    app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-        {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: bedrag,
-            quantity: 1,
-        },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}/success.html`,
-        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  var checkoutButton = document.getElementById('submit');
+  checkoutButton.addEventListener('click', function () {
+    /*
+     * When the customer clicks on the button, redirect
+     * them to Checkout.
+     */
+    stripe.redirectToCheckout({
+      lineItems: [{price: 'price_1Ot7VmHQSNvOpQERifodpXXv', quantity: 1}],
+      mode: 'payment',
+      payment_method_types: ['card', 'ideal'],
+      successUrl: 'https://repsac-casper.github.io/the-safe-app/geld.html',
+      cancelUrl: 'https://repsac-casper.github.io/the-safe-app/geld.html',
+    })
+    .then(function (result) {
+      if (result.error) {
+        /*
+         * If `redirectToCheckout` fails due to a browser or network
+         * error, display the localized error message to your customer.
+         */
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
     });
-
-    res.redirect(303, session.url);
-    });
-
-    app.listen(4242, () => console.log('Running on port 4242'));
-});
+  });
+})();
